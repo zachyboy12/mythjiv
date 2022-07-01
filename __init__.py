28c0474792a96b00a1705d266f635626cb8d84a9
Tumo <104463761+zachyboy12@users.noreply.github.com>
2022-07-01 18:04:41 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,120 +1,88 @@
 """
 A web framework to build the back-end.
 """
-    
 
-class API:
-    """
-    Creates an API.
-    """
-    
+
+class Myth:
+        
     
     def __init__(self) -> None:
         self.routes = None
+        
+        
+    def add_route(self, route: str, app):
+        def ignore(request):
+            class HTTPResponseDummy:
+                def __init__(self) -> None:
+                    self.body = ''
+            return HTTPResponseDummy()
+        if self.routes == None:
+            self.routes = {route: app, '/favicon.ico': ignore}
+            self.routes[route[0:-1]] = app
+        else:
+            if route in self.routes:
+                raise AssertionError(f'Route ({route}) can only be used once.')
+            self.routes[route] = app
+            if route[-1] == '/':
+                self.routes[route[0:-1]] = app
+            else:
+                self.routes[route + '/'] = app
     
     
     def route(self, route: str):
-        """
-        Routes app to the decorated function.
-
-        Args:
-            route (str): Route name for function to be wrapped to.
-        """
-        def decor(app):
-            if self.routes is None:
-                self.routes = {route: app}
-            elif self.routes is not None:
-                if self.routes.get(route, None) != None:
-                    raise AssertionError('Such route exists.')
-                self.routes[route] = app
-        return decor
-    
-    
-    def connect_route(self, route_function, route: str):
-        """
-        Routes app to the route_function. Clone of route, but not a decorator.
-
-        Args:
-            route_function (function): Function to connect to route.
-            route (str): Route to be connected to route_function.
-        """
-        if self.routes is None:
-                self.routes = {route: route_function}
-        elif self.routes is not None:
-            self.routes[route] = route_function
+        def wrapper(app):
+            self.add_route(route, app)
+        return wrapper
         
+        
+    def defult_exception_handler(self, request, exception='?'):
+        class DummyHTTPResponse:
+            """
+            Creates a dummy HTTP response.
+            """
+            def __init__(self, body: str):
+                self.body = body
+                self.HTTP_version = '1.1'
+                self.status = '200 OK'
+        return DummyHTTPResponse('An internal error occured; a ' + str(exception) + ' error.')
     
     
-    def runserver(self, host='127.0.0.1', port=8000, html_404='Page not found. Maybe you mispelled the url?', frameworkname=None):
-        """
-        Makes SimpleServer serve forever.
-        """
-        import socket, logging, time, inspect
-
-        if self.routes is None:
-            raise ValueError('No routes registered.')
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind((host, port))
-        server_socket.listen(1)
-        requests = ''
-        if frameworkname is None:
-            pass
-        else:
-            print(f'% {frameworkname} %')
-        print(f'Listening on: http://{host}:{port} ...')
-        logging.warn('\033[93m CANNOT BE USED FOR PRODUCTION. USE A WSGI SERVER \033[0m')
-        while True:
+    def defult_404_handler(self, request):
+        class DummyHTTPResponse:
+            """
+            Creates a dummy HTTP response.
+            """
+            def __init__(self, body: str):
+                self.body = body
+                self.HTTP_version = '1.1'
+                self.status = '200 OK'
+        return DummyHTTPResponse('URL not found: ' + request['PATH_INFO'])
+    
+    
+    def configure_exception_handler(self, new_handler):
+        self.defult_exception_handler = new_handler
+        
+        
+    def configure_404_handler(self, new_handler):
+        self.defult_404_handler = new_handler
+        
+        
+    def runserver(self, host='localhost', port=8000, poll_seconds=0.0):
+        from wsgiref.simple_server import make_server
+        def app(environment, start_response):
+            start_response('200 OK', headers=[])
             try:
-                client_connection, client_address = server_socket.accept()
-                request = client_connection.recv(1024).decode()
-                headers = request.split('\n')
-                request_name = headers[0].split()[1]
-                has_response = False
-                requests += '\n' + request
-                response = ''
-                print(time.strftime('\033[93m[%m/%d/%Y %H:%M:%S] \033[0m- ') + request.split('\n')[0])
-                for theroute in self.routes:
-                    if request_name == theroute or request_name == theroute[0:-1] or request_name == theroute + '/':
-                        has_response = True
-                        response = f'HTTP/{self.routes.get(theroute, "")().HTTP_version} {self.routes.get(theroute, "")().status}\n\n{self.routes.get(theroute, "")().body}'
-                    else:
-                        continue
-                if has_response == True:
-                    pass
-                else:
-                    response = f'HTTP/{self.routes.get(theroute, "")().HTTP_version} {self.routes.get(theroute, "")().status}\n\n{html_404}'
-                client_connection.sendall(response.encode())
-                client_connection.close()
-            except KeyboardInterrupt:
-                break
-        server_socket.close()
-        return f'\nRequests:\n' + requests
-    
-    
-class Myth(API):
-    """
-    Creates an API. Clone of the API class.
-    """
-    pass
-
-
-class Template:
-    """
-    Creates a Template.
-    """
-    
-    
-    def __init__(self, myth_or_api, begin_tag='[% ', end_tag=' %]') -> None:
-        self.context = myth_or_api.routes.get()()
-        self.bt = begin_tag
-        self.et = end_tag
-        
-        
-    def render_values(self, names_and_values: dict):
-        for name in names_and_values:
-            tag = self.bt + name + self.et
-            self.context.replace(tag, names_and_values.get(name, name))
+                return [bytes(str(self.routes[environment['PATH_INFO']](environment).body).encode())]
+            except KeyError:
+                return [bytes(str(self.defult_404_handler(environment).body).encode())]
+            except Exception as e:
+                return [bytes(str(self.defult_exception_handler(environment, exception=e).body).encode())]
+        print('Serving on http://' + host + ':' + str(port) + ' ...')
+        server = make_server(host, port, app)
+        try:
+            server.serve_forever(poll_seconds)
+        except:
+            server.shutdown()
 
 
 class HTTPResponse:
@@ -123,7 +91,7 @@ class HTTPResponse:
     """
     
     
-    def __init__(self, body: str):
+    def __init__(self, body):
         self.body = body
         self.HTTP_version = '1.1'
         self.status = '200 OK'
@@ -141,14 +109,14 @@ class Database:
         except:
             raise FileNotFoundError('Database file does not exist.')
         self.__original = open(database_file).read()
-        self.database_content = []
+        self.__database_content = []
         self.database_file = database_file
         for row in open(database_file).read().split('\n'):
-            self.database_content.append(row.split(' '))
+            self.__database_content.append(row.split(' '))
         
         
     def get_row(self, row_number: int) -> list:
-        return self.database_content[row_number - 1]
+        return self.__database_content[row_number - 1]
     
     
     def get_column(self, row_number: int, column_number: int) -> str:
@@ -158,19 +126,19 @@ class Database:
     def update_row(self, row_number: int, update: str) -> None:
         content = open(self.database_file).read()
         open(self.database_file, 'w').write(content.replace(content.split('\n')[row_number - 1], update))
-        self.database_content = []
+        self.__database_content = []
         self.database_file = self.database_file
         for row in open(self.database_file).read().split('\n'):
-            self.database_content.append(row.split(' '))
+            self.__database_content.append(row.split(' '))
         
         
     def update_column(self, row_number: int, column_number: int, update: str) -> None:
         content = open(self.database_file).read()
         open(self.database_file, 'w').write(content.replace(content.split('\n')[row_number - 1][column_number - 1], update))
-        self.database_content = []
+        self.__database_content = []
         self.database_file = self.database_file
         for row in open(self.database_file).read().split('\n'):
-            self.database_content.append(row.split(' '))
+            self.__database_content.append(row.split(' '))
         
         
     def delete_row(self, row_number: int) -> None:
@@ -183,3 +151,21 @@ class Database:
         
     def undo_all_changes(self):
         open(self.database_file).write(self.__original)
+        
+        
+    def get_all(self):
+        return self.__database_content
+
+
+class TemplateEnvironment:
+    
+    
+    def __init__(self, response: HTTPResponse, begin_tag='[& ', end_tag=' &]') -> None:
+        self.response = response
+        self.begin_tag = begin_tag
+        self.end_tag = end_tag
+        
+        
+    def render_values(self, names_and_values: dict):
+        for name in names_and_values:
+            self.response.body = self.response.body.replace(self.begin_tag + name + self.end_tag, names_and_values.get(name))
